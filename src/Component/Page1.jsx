2988d66b7b908344e5ac7d2968ff9b67c05c7fc9
angga <anggaairulr@gmail.com>
2023-06-22 13:15:57 +0700

    update beranda

--- src/Component/Page1.jsx
+++ src/Component/Page1.jsx
@@ -1,5 +1,9 @@
 import "./Component CSS/Page1.css";
-import hero from "../../src/assets/Img/hero.png"
+import hero from "../../src/assets/Img/hero.png";
+import ingredients1 from "../../src/assets/Img/spices1.png";
+import ingredients2 from "../../src/assets/Img/spices2.png";
+import ingredients3 from "../../src/assets/Img/spices3.png";
+import imgbox from "../../src/assets/Img/nasilemak.jpg";
 
 // import Subpage1 from "./Subpage1"
 import Back from "../assets/Img/Makanan.jpg";
@@ -8,7 +12,6 @@ function Page1() {
   return (
     <div className="beranda">
       <div className="sub-beranda">
-
         <div className="beranda-kiri">
           <div className="headline-hero">
             <li>
@@ -21,12 +24,21 @@ function Page1() {
               <span>Harapan!</span>
             </li>
           </div>
+
+          <div className="ingredients">
+            <img className="ingredients-1" src={ingredients1} alt="" />
+            <img className="ingredients-2" src={ingredients2} alt="" />
+            <img className="ingredients-3" src={ingredients3} alt="" />
+          </div>
+
+          <div className="img-kiri">
+            <img className="img-box" src={imgbox} alt="" />
+            <h1>Tingkatkan Acara Anda dengan Keajaiban Kuliner Kami!</h1>
+          </div>
         </div>
 
         <div className="beranda-kanan">
-         <div className="hero">
-            <img className="hero-img" src={hero} alt="" />
-         </div>
+          <img className="hero-img" src={hero} alt="" />
         </div>
       </div>
     </div>
